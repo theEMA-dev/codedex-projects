7ec80b4edee30642d281bfc6ca81892a5a67c23c
theEMA <61084889+theEMA-dev@users.noreply.github.com>
2024-08-11 22:55:52 +0300

    added code buttons

--- leap-year-checker/project.js
+++ leap-year-checker/project.js
@@ -1,5 +1,3 @@
-console.log("Welcome to Leap Year Checker\nby theEMA - Made for Codedex\n==============================\nUse the checkLeapYear() function to check\nif a year is a leap year or not.");
-
 function checkLeapYear(year) {
     if (year % 4 === 0) {
         if (year % 100 === 0) {
